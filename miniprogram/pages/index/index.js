426cd09b064600ef9749617aac9a44c034716ae2
金辉 <1303101041@qq.com>
2025-06-30 10:20:54 +0800

    jh

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -14,14 +14,6 @@ Page({
       { name: "小憩区", icon: "/assets/11.png" },
       { name: "电波室", icon: "/assets/12.png" },
       { name: "YoYo宠物", icon: "/assets/13.png" },
-    ],
-    tabs: [
-      { name: "目标", icon: "/assets/tab1.png" },
-      { name: "自习号", icon: "/assets/tab2.png" },
-      { name: "专注", icon: "/assets/tab3.png" },
-      { name: "计划", icon: "/assets/tab4.png" },
-      { name: "Yo家", icon: "/assets/tab5.png" },
-      { name: "我的", icon: "/assets/tab6.png" },
     ]
   }
 })
